1400ccbd726c67736b179b99b6c96fdd81abc96c
khachik288-web <khachik228@gmail.com>
2026-09-10 00:21:31 +0400

    nor proekt

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,19 @@ import { getDatabase } from "firebase/database";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvddvMoXHy8UQYYZpbovSsmEbKVfIr7ng",
-  authDomain: "team-telecom-arm-79182.firebaseapp.com", 
-  databaseURL: "https://team-telecom-arm-default-rtdb.firebaseio.com",
-  projectId: "team-telecom-arm-79182",
-  storageBucket: "team-telecom-arm-79182.firebasestorage.app",
-  messagingSenderId: "114441898118",
-  appId: "1:114441898118:web:2b7e2ed7da088dc8dbe907",
-  measurementId: "G-0RKB66JLWB"
+  apiKey: "AIzaSyC3GhPLKOh3aZ2DN8rDUYRazXkvuZ8LRd4",
+  authDomain: "new-team-telecom-67513.firebaseapp.com",
+  databaseURL: "https://new-team-telecom-67513-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "new-team-telecom-67513",
+  storageBucket: "new-team-telecom-67513.firebasestorage.app",
+  messagingSenderId: "653071595875",
+  appId: "1:653071595875:web:0436c1c653bd1e72dd7b56",
+  measurementId: "G-SQLFRNW2HV"
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const analytics = getAnalytics(app);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
